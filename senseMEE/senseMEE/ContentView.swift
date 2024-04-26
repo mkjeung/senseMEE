74e9b44e45b96d8befdad7f8bb52ff46b1a6b9ad
mkjeung <mkjeung@uchicago.edu>
2024-04-26 15:56:27 -0500

    created project

---
+++ senseMEE/senseMEE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  senseMEE
+//
+//  Created by Matthew Jeung on 4/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ senseMEE/senseMEE/senseMEEApp.swift
@@ -0,0 +1,17 @@
+//
+//  senseMEEApp.swift
+//  senseMEE
+//
+//  Created by Matthew Jeung on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct senseMEEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
